8cdd0cdde60e0f674c11c660ccaf1691daf85cf3
mbaggott <5018715+mbaggott@users.noreply.github.com>
2024-05-24 12:41:55 +1000

    added new URL link for everwarm-react-ts site to "Web" page

--- src/pages/web/web.tsx
+++ src/pages/web/web.tsx
@@ -41,7 +41,7 @@ const Web = () => {
                                 <article className="inline projectLabel wordWrap projectPadding">Description:</article><article className="inline wordWrap projectDescription projectPadding">Michaels first attempt at building a web page. While design was not being judged in this assessment, the site does demonstrate the use of three elements of web development, HTML, CSS and Javascript. Simulating the functions of sales and delivery for a retail business, concepts used include an HTML form, user input validation, and calculations with Javascript. All processing is performed client-side in the browser.</article>
                             </section>
                             <section>
-                                <article className="inline projectLabel wordWrap projectPadding">Link to Site:</article><article className="inline projectDescription projectPadding"><a className="projectLink" href = "everwarm/" target="_blank">Everwarm Fuel Merchants</a></article>
+                                <article className="inline projectLabel wordWrap projectPadding">Link to Site:</article><article className="inline projectDescription projectPadding"><a className="projectLink" href = "/portfolio/everwarm-react-ts/" target="_blank">Everwarm Fuel Merchants</a></article>
                             </section>
                             <section>
                                 <article className="inline projectLabel wordWrap projectPadding">Browse Project:</article><article className="inline projectDescription projectPadding">Go to <HashLink onClick={() => handleClick('2')} className="projectLink" to='/browse#everwarm'>Browse</HashLink>  Page</article>
