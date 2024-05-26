639055241e964ca2e8e4476eb2df0adb1fa77d55
mbaggott <5018715+mbaggott@users.noreply.github.com>
2024-05-27 08:04:56 +1000

    Added dummy secrets file

---
+++ secrets.php
@@ -0,0 +1,7 @@
+<?php
+	/* Goes outside the webroot. Must be pointed to by the /scripts/dig251-react-ts/contactemail.php file */
+	/* recapycha secret key */
+	/* email password on host */
+	$recaptcha='';
+	$emailpass='';
+?>
